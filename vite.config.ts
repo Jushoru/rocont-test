f0eeba0a66346e73c569fd98a1b7be10d87e6ed3
Jushoru <smirnov.kirill.kaduy@gmail.com>
2025-09-19 11:34:49 +0300

    build: deployed prject on gh-pages

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
   plugins: [vue()],
+  base: '/rocont-test',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
